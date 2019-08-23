c0e1277766e1cee1fef195a924450e07e9d43fa6
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-23 23:13:56 +0700

    Add files via upload

---
+++ ZOMBIES.sh
@@ -0,0 +1,220 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo 
+echo
+echo
+echo $red" @@@@@@@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@  @@@  "
+sleep 1
+echo $red"@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@  "
+sleep 1
+echo $yellow"!@@        @@!  @@@  @@!       @@!       @@!@!@@@  "
+sleep 1
+echo $yellow"!@!        !@!  @!@  !@!       !@!       !@!!@!@!  "
+sleep 1
+echo $green"!@! @!@!@  @!@!!@!   @!!!:!    @!!!:!    @!@ !!@!  "
+sleep 1
+echo $green"!!! !!@!!  !!@!@!    !!!!!:    !!!!!:    !@!  !!!  "
+sleep 1
+echo $red":!!   !!:  !!: :!!   !!:       !!:       !!:  !!!  "
+sleep 1
+echo $red":!:   !::  :!:  !:!  :!:       :!:       :!:  !:!  "
+echo $yellow" ::: ::::  ::   :::   :: ::::   :: ::::   ::   ::  "
+echo $yellow" :: :: :    :   : :  : :: ::   : :: ::   ::    :   "
+echo
+echo $green"                                                                    "
+echo $green"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@   @@@@@@   "
+echo $green"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  @@@@@@@   "
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !@@       "
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !@!       "
+echo $yellow"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    !!@@!!    "
+echo $yellow"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:     !!@!!!   "
+echo $green" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:            !:!  "
+echo $green":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:           !:!   "
+echo $green" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  :::: ::   "
+echo $green": :: : :   : :  :    :      :    :: : ::   :    : :: ::   :: : :    "
+echo
+echo $indigo
+date
+echo $red"___________________________________________________"
+echo $red" [+]Author by      : GREEN ZOMBIES "
+echo $yellow" [+]Team           : AGEN SEA         "
+echo $yellow" [+]Thanks to      : MR_81"
+echo $green" [+]Thanks to      : MR_ARIANIMOUS"
+echo $green" [+]Thanks to      : MR_XZ.6"
+echo $red" [+]Thanks to      : B3kT0509"
+echo $red" [+]Thanks to      : MR_VIRUS"
+echo $yellow" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $green"___________________________________________________"
+echo
+echo
+sleep 3
+echo
+echo $red"___________________________________"
+echo $yellow"[1]"$yellow"MEMBUAT TERMUX ENAK "
+echo $green"___________________________________"
+echo $red"___________________________________"
+echo $yellow"[2]"$yellow"CARA PAKAI NYA "
+echo $green"___________________________________"
+echo "\033[34;1m  99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m PILIH DENGAN BIJAK!\033[32;1m"
+read -p " GREENZOMBIES$ " pilih
+if [ $pilih = 1 ]
+then
+clear
+echo
+echo
+echo
+echo $red" @@@@@@@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@  @@@  "
+sleep 1
+echo $red"@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@  "
+sleep 1
+echo $yellow"!@@        @@!  @@@  @@!       @@!       @@!@!@@@  "
+sleep 1
+echo $yellow"!@!        !@!  @!@  !@!       !@!       !@!!@!@!  "
+sleep 1
+echo $green"!@! @!@!@  @!@!!@!   @!!!:!    @!!!:!    @!@ !!@!  "
+sleep 1
+echo $green"!!! !!@!!  !!@!@!    !!!!!:    !!!!!:    !@!  !!!  "
+sleep 1
+echo $red":!!   !!:  !!: :!!   !!:       !!:       !!:  !!!  "
+sleep 1
+echo $red":!:   !::  :!:  !:!  :!:       :!:       :!:  !:!  "
+echo $yellow" ::: ::::  ::   :::   :: ::::   :: ::::   ::   ::  "
+echo $yellow" :: :: :    :   : :  : :: ::   : :: ::   ::    :   "
+echo
+echo $green"                                                         "
+echo $green"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@ "
+echo $green"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@ "
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !"
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !"
+echo $yellow"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!  "
+echo $yellow"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:  "
+echo $green" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:      "
+echo $green":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:      "
+sleep 2
+echo $green" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: :::: "
+echo $green": :: : :   : :  :    :      :    :: : ::   :    : :: ::  "
+echo
+echo $indigo
+date
+echo $red"___________________________________________________"
+echo $red" [+]Author by      : GREEN ZOMBIES "
+echo $yellow" [+]Team           : AGEN SEA         "
+sleep 1
+echo $yellow" [+]Thanks to      : MR_81"
+echo $green" [+]Thanks to      : MR_ARIANIMOUS"
+echo $green" [+]Thanks to      : MR_XZ.6"
+echo $red" [+]Thanks to      : B3kT0509"
+echo $red" [+]Thanks to      : MR_VIRUS"
+echo $yellow" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $green"___________________________________________________"
+echo "\033[34;1mLoading.....!!!"
+echo
+sleep 1
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install openssl
+pip2 install urllib3 chardet certivi idna requests
+pkg install curl
+pkg install libcurl
+pip2 install mechanize
+pkg install git
+pkg install wget
+pip2 install bs4
+pkg install php
+pip2 install --upgrade pip
+pkg install nodejs git
+pkg install mechanize
+pkg install python2-der
+pkg install clang
+pkg install w3m
+pkg install figlet
+pkg install neofetch
+pkg install toilet
+pip2 install requests mechanize
+pip2 install -r requeriments
+pkg install perl
+pkg install nano
+pkg install ruby
+pkg install cowsay
+pkg install lolcat
+pkg install vim
+pkg install termcolor
+pkg install gem
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+echo
+echo $red" @@@@@@@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@  @@@  "
+sleep 1
+echo $red"@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@  "
+sleep 1
+echo $yellow"!@@        @@!  @@@  @@!       @@!       @@!@!@@@  "
+echo $yellow"!@!        !@!  @!@  !@!       !@!       !@!!@!@!  "
+sleep 1
+echo $green"!@! @!@!@  @!@!!@!   @!!!:!    @!!!:!    @!@ !!@!  "
+sleep 1
+echo $green"!!! !!@!!  !!@!@!    !!!!!:    !!!!!:    !@!  !!!  "
+sleep 1
+echo $red":!!   !!:  !!: :!!   !!:       !!:       !!:  !!!  "
+sleep 1
+echo $red":!:   !::  :!:  !:!  :!:       :!:       :!:  !:!  "
+echo $yellow" ::: ::::  ::   :::   :: ::::   :: ::::   ::   ::  "
+echo $yellow" :: :: :    :   : :  : :: ::   : :: ::   ::    :   "
+echo $green"                                                         "
+echo $green"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@ "
+echo $green"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@ "
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !"
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !"
+sleep 1
+echo $yellow"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!  "
+echo $yellow"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:  "
+echo $green" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:      "
+echo $green":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:      "
+echo $green" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: :::: "
+echo $green": :: : :   : :  :    :      :    :: : ::   :    : :: ::  "
+sleep 1
+echo
+echo $indigo
+date
+echo $red"___________________________________________________"
+echo $red" [+]Author by      : GREEN ZOMBIES "
+echo $yellow" [+]Team           : AGEN SEA         "
+echo $yellow" [+]Thanks to      : MR_81"
+echo $green" [+]Thanks to      : MR_ARIANIMOUS"
+echo $green" [+]Thanks to      : MR_XZ.6"
+echo $red" [+]Thanks to      : B3kT0509"
+echo $red" [+]Thanks to      : MR_VIRUS"
+echo $yellow" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $green"___________________________________________________"
+echo
+sleep 2
+echo
+echo $yellow" CARA PAKAI NYA GAMPANG NJIR:V"
+sleep 1
+echo $yellow" KALAU KALIAN UDAH INSTALL NOMER 1"
+sleep 1
+echo $yellow" KETIKA KALIAN NEMU SC TERMUX LAGI"
+sleep 1
+echo $yellow" GAK USAH KETIK2 pkg instal apa lah"
+echo $yellow"KALIAN BISA LANGSUNG KETIK git clone "
+sleep 1
+echo $yellow" terus link github nya"
+echo $yellow"udah faham kan?:v"
+sleep 1
+echo $yellow"KALO GAK FAHAM TIDUR AJA:V"
+fi
+
+if
+
